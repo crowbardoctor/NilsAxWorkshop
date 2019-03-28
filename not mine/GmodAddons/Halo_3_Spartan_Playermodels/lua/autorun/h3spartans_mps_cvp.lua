7508a7c8f747d9334e74e11d2b299dea08d35fec
crowbardoctor <42645295+crowbardoctor@users.noreply.github.com>
2019-03-28 09:18:49 +0100

    Add files via upload

---
+++ not mine/GmodAddons/Halo_3_Spartan_Playermodels/lua/autorun/h3spartans_mps_cvp.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Spartan (Halo 3 MP)", "models/player/lordvipes/h3_spartans_mps/h3spartan_mps_cvp.mdl" );
+player_manager.AddValidHands( "Spartan (Halo 3 MP)", "models/player/lordvipes/h3_spartans_mps/Arms/h3spartan_mps_arms_cvp.mdl", 0, "00000000" )
+list.Set( "PlayerOptionsModel", "Spartan (Halo 3 MP)", "models/player/lordvipes/h3_spartans_mps/h3spartan_mps_cvp.mdl" );
